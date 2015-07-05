f1b2924c34704e8737fa0b5f17128f3ed3f5a441
csimon1 <charly.simon.1@etsmtl.net>
2015-07-05 19:28:17 -0400

    add autogenerate exception to log it

---
+++ election/src/main/java/console/ConsoleService.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package console;
+
+import service.Service;
+
+/**
+ * @author MisterTim
+ *
+ */
+public class ConsoleService extends Service {
+
+	
+	public ConsoleService(String serviceName) {
+		super(serviceName);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see service.Service#loopAction()
+	 */
+	@Override
+	public void loopAction() {
+		
+	}
+
+}
