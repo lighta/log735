26afbd9a46503a55fcd04da7977fa861cf8210bb
lighta <lightaisme@gmail.com>
2015-07-31 00:12:08 -0400

    Update env.sh

--- jbittorrentapi-v1.0/env.sh
+++ jbittorrentapi-v1.0/env.sh
@@ -2,3 +2,10 @@
 
 CLASSPATH=jBittorrentAPI.jar:trackerBT.jar:ext/ant.jar:ext/freemarker.jar:ext/groovy.jar:ext/jaxen-core.jar:ext/jaxen-jdom.jar:ext/jdom.jar:ext/kxml.jar:ext/saxpath.jar:ext/simple-upload-0.3.4.jar:ext/velocity.jar:extxalan.jar:ext/xerces.jar:ext/xml-apis.jar
 export CLASSPATH
+
+echo '10.196.113.23 Lighta_fedo' >> ~/.hosts
+export HOSTALIASES=~/.hosts
+
+mkdir torrent
+scp lighta@Lighta_fedo:/home/lighta/Documents/ETS/LOG735/jbittorrentapi-v1.0/torrent/* ./torrent/
+scp lighta@Lighta_fedo:/home/lighta/Documents/ETS/LOG735/jbittorrentapi-v1.0/*.jar ./
